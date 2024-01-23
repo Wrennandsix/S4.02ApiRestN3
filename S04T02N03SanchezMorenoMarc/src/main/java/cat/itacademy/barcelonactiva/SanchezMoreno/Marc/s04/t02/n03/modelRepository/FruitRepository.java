b37b730d6f9b82332f9471c3c199c2c4c53f0bd4
Marc Sanchez Moreno <127627232+Wrennandsix@users.noreply.github.com>
2024-01-23 13:42:42 +0100

    Delete S04T02N03SanchezMorenoMarc/src/main/java/cat/itacademy/barcelonactiva/SanchezMoreno/Marc/s04/t02/n03/modelRepository directory

--- S04T02N03SanchezMorenoMarc/src/main/java/cat/itacademy/barcelonactiva/SanchezMoreno/Marc/s04/t02/n03/modelRepository/FruitRepository.java
+++
@@ -1,9 +0,0 @@
-package cat.itacademy.barcelonactiva.SanchezMoreno.Marc.s04.t02.n03.modelRepository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import cat.itacademy.barcelonactiva.SanchezMoreno.Marc.s04.t02.n03.modelDomain.Fruit;
-
-public interface FruitRepository extends MongoRepository<Fruit, Integer> {
-
-}
